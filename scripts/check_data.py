ec7214b9f30c6cebe1a0698363618d2b03ec4c0d
workualemu <alemu.worku@gmail.com>
2025-08-10 11:21:29 -0400

    Update check_data.py

--- scripts/check_data.py
+++ scripts/check_data.py
@@ -1,11 +1,11 @@
+# scripts/check_data.py
 from sdg.open_sdg import open_sdg_check
-from build_data import _clean_data  # or duplicate the function
+from build_data import _clean_data
 
-validation_successful = open_sdg_check(config="config_data.yml", alter_data=_clean_data)
-# Validate the indicators.
-# validation_successful = open_sdg_check(config='config_data.yml')
-
-# If everything was valid, perform the build.
-if not validation_successful:
-    print("Validation errors found (proceeding anyway).")
-    # raise Exception('There were validation errors. See output above.')
+if __name__ == "__main__":
+    ok = open_sdg_check(
+        config="config_data.yml",
+        alter_data=_clean_data,   # run the same cleaner during validation
+    )
+    if not ok:
+        print("Validation errors found (proceeding anyway).")
